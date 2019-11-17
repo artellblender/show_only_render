cd0238b1dbb2e77d9fe333e431cc19a09d5ef900
artellblender <lucky3d1@gmail.com>
2019-11-17 18:29:35 +0100

    initial commit

---
+++ show_only_render.py
@@ -0,0 +1,100 @@
+import bpy
+
+bl_info = {
+    'name': "Show Only Render",
+    'description': "Toggle to show only rendered objects in the viewport",
+    'author': "Artell",
+    "version": (1, 10, 10),
+    'blender': (2, 80, 0),
+    'location': "Tools Panel (T) in viewport",
+    'wiki_url': "",
+    'category': "Render",
+    }
+
+
+class SOR_OT_toggle_visibility(bpy.types.Operator):
+    bl_description = "Toggle to show only rendererable objects"
+    bl_idname = 'sor.toggle'
+    bl_label = "show_only_render"
+    bl_options = set({'REGISTER', 'UNDO'})
+
+   
+    """
+    @classmethod
+    def poll(self, context):
+        return bpy.data.materials
+    """
+    def invoke(self, context, event):
+        return self.execute(context)
+
+    def execute(self, context):
+        if self.mute == 'toggle':
+            mute = None
+        elif self.mute == 'off':
+            mute = False
+        elif self.mute == 'on':
+            mute = True
+
+        for mat in bpy.data.materials:
+            nodes = getattr(mat.node_tree, 'nodes', [])
+            for node in nodes:
+                if isinstance(node, bpy.types.ShaderNodeNormalMap):
+                    if mute is None:
+                        mute = node.mute
+                    node.mute = not mute
+
+        return {'FINISHED'}
+
+
+class SOR_PT_menu(bpy.types.Panel):
+    bl_category = ""
+    bl_label = ""
+    bl_options = set({'HIDE_HEADER'})
+    bl_region_type = 'UI'
+    bl_space_type = 'VIEW_3D'
+    bl_category = "View"    
+
+    @classmethod
+    def poll(self, context):
+        return True
+
+    def draw(self, context):
+        layout = self.layout        
+        layout.prop(bpy.context.scene, "sor_show_only_render", text="Show Only Render")
+
+    
+def update_show_only_render(self, context):    
+    for obj in bpy.context.scene.objects:  
+        if context.scene.sor_show_only_render:
+            obj.hide_set(obj.hide_render)    
+        else:
+            obj.hide_set(False)    
+           
+                
+            
+            
+            
+classes = (
+    SOR_OT_toggle_visibility,
+    SOR_PT_menu,
+    )
+    
+    
+def register():   
+    from bpy.utils import register_class
+
+    for cls in classes:
+        register_class(cls)
+    
+    bpy.types.Scene.sor_show_only_render = bpy.props.BoolProperty(name="Show Only Render", default = False, description="Show only renderable objects", update=update_show_only_render)
+
+    
+def unregister():   
+    from bpy.utils import unregister_class
+
+    for cls in reversed(classes):
+        unregister_class(cls)
+    
+    del bpy.types.Scene.sor_show_only_render
+
+
